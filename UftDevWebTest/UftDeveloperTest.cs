84d5fc5cd1924a6758bcec9da6dfdcda471975fb
zirock <zirock699@gmail.com>
2023-10-02 13:31:56 +0000

    Dummy tests

--- UftDevWebTest/UftDeveloperTest.cs
+++ UftDevWebTest/UftDeveloperTest.cs
@@ -20,68 +20,109 @@ namespace UftDevWebTest
         {
 
         }
+        //[TestMethod]
+
+        //public void MyLoginTest()
+        //{
+        //    IBrowser browser = BrowserFactory.Launch(BrowserType.Chrome);
+        //    browser.Navigate("http://advantageonlineshopping.com/#/");
+
+        //    for (int i=1;i <11; i++)
+        //    {
+        //        browser.OpenNewTab();
+
+        //    }
+
+        //    Console.WriteLine("Testing...*************************");
+
+
+        //    var userMenuLink = browser.Describe<ILink>(new LinkDescription
+        //    {
+        //        InnerText = @"My account My orders Sign out ",
+        //        TagName = @"A"
+        //    });
+        //    userMenuLink.Click();
+
+
+        //    var usernameEditField = browser.Describe<IEditField>(new EditFieldDescription
+        //    {
+        //        Name = @"username",
+        //        TagName = @"INPUT",
+        //        Type = @"text"
+        //    });
+        //    usernameEditField.SetValue("RandomUserName");
+
+        //    var passwordEditField = browser.Describe<IEditField>(new EditFieldDescription
+        //    {
+        //        Name = @"password",
+        //        TagName = @"INPUT",
+        //        Type = @"password"
+        //    });
+        //    passwordEditField.SetSecure("6514481c7a0efe6e7834029e51d2341af2856953ce0d6971a80d6aec61a3a4fe59ff");
+
+        //    var signInBtnButton = browser.Describe<IButton>(new ButtonDescription
+        //    {
+        //        ButtonType = @"button",
+        //        Name = @"SIGN IN",
+        //        TagName = @"BUTTON"
+        //    });
+        //    signInBtnButton.Click();
+
+        //    signInBtnButton.Click();
+
+        //    var signInResultMessageWebElement = browser.Describe<IWebElement>(new WebElementDescription
+        //    {
+        //        TagName = @"LABEL",
+        //        InnerText = @"Incorrect user name or password."
+        //    });
+
+        //    Verify.AreEqual(@"Incorrect user name or password.", signInResultMessageWebElement.InnerText, "Verification", "Verify property: innerText");
+
+
+        //}
+
+        private Random random = new Random();
+
         [TestMethod]
-
-        public void MyLoginTest()
+        public void VerifyUsernameIsNotEmpty()
         {
-            IBrowser browser = BrowserFactory.Launch(BrowserType.Chrome);
-            browser.Navigate("http://advantageonlineshopping.com/#/");
-
-            for (int i=1;i <11; i++)
-            {
-                browser.OpenNewTab();
-
-            }
-
-            Console.WriteLine("Testing...*************************");
-
-            
-            var userMenuLink = browser.Describe<ILink>(new LinkDescription
-            {
-                InnerText = @"My account My orders Sign out ",
-                TagName = @"A"
-            });
-            userMenuLink.Click();
-
-
-            var usernameEditField = browser.Describe<IEditField>(new EditFieldDescription
-            {
-                Name = @"username",
-                TagName = @"INPUT",
-                Type = @"text"
-            });
-            usernameEditField.SetValue("RandomUserName");
-
-            var passwordEditField = browser.Describe<IEditField>(new EditFieldDescription
-            {
-                Name = @"password",
-                TagName = @"INPUT",
-                Type = @"password"
-            });
-            passwordEditField.SetSecure("6514481c7a0efe6e7834029e51d2341af2856953ce0d6971a80d6aec61a3a4fe59ff");
-
-            var signInBtnButton = browser.Describe<IButton>(new ButtonDescription
-            {
-                ButtonType = @"button",
-                Name = @"SIGN IN",
-                TagName = @"BUTTON"
-            });
-            signInBtnButton.Click();
-
-            signInBtnButton.Click();
-
-            var signInResultMessageWebElement = browser.Describe<IWebElement>(new WebElementDescription
-            {
-                TagName = @"LABEL",
-                InnerText = @"Incorrect user name or password."
-            });
-
-            Verify.AreEqual(@"Incorrect user name or password.", signInResultMessageWebElement.InnerText, "Verification", "Verify property: innerText");
-
-
+            int testValue = random.Next(0, 2); // Generates 0 or 1
+            string username = testValue == 1 ? "JohnDoe" : "";
+            Assert.IsFalse(string.IsNullOrEmpty(username), "Username should not be empty");
         }
 
-        
+        [TestMethod]
+        public void VerifyPasswordLength()
+        {
+            int testValue = random.Next(3, 10); // Generates a number between 3 and 9
+            string password = new string('*', testValue);
+            Assert.IsTrue(password.Length >= 6, $"Password length {password.Length} is less than the minimum required length of 6");
+        }
+
+        [TestMethod]
+        public void ValidateSuccessfulLogin()
+        {
+            int testValue = random.Next(0, 2); // Generates 0 or 1
+            bool loginSuccessful = testValue == 1;
+            Assert.IsTrue(loginSuccessful, "Login should be successful");
+        }
+
+        [TestMethod]
+        public void CheckForTwoFactorAuthentication()
+        {
+            int testValue = random.Next(0, 2); // Generates 0 or 1
+            bool twoFactorEnabled = testValue == 1;
+            Assert.IsTrue(twoFactorEnabled, "Two-factor authentication should be enabled");
+        }
+
+        [TestMethod]
+        public void VerifyAccountLockAfterFailedAttempts()
+        {
+            int failedAttempts = random.Next(1, 6); // Generates a number between 1 and 5
+            Assert.IsTrue(failedAttempts < 5, $"Account should not be locked, but had {failedAttempts} failed attempts");
+        }
+
+
 
         [TestCleanup]
         public void TestCleanup()
