5ad0c7fffa1d26c098cc4e1df92a4c88398fdb63
zirock <zirock699@gmail.com>
2023-10-02 12:15:13 +0000

    test

--- UftDevWebTest/UftDeveloperTest.cs
+++ UftDevWebTest/UftDeveloperTest.cs
@@ -33,6 +33,8 @@ namespace UftDevWebTest
 
             }
 
+            Console.WriteLine("Testing...*************************");
+
             
             var userMenuLink = browser.Describe<ILink>(new LinkDescription
             {
